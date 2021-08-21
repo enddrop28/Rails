9d6b80ebf66fd608d5fc74dd42d3c8f1a3da22d4
EC2 Default User <ec2-user@ip-172-31-86-81.ec2.internal>
2021-08-21 18:54:31 +0000

    [Finish]2-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -7,11 +7,12 @@ class TodolistsController < ApplicationController
 
 
   def create
-    # １. データを新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    list.save
-   # 詳細画面へリダイレクト
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
 
   end
 
